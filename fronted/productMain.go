82ab416c67c8fcb4ecd564eb748e9d0bc7cc0c73
pimg-wu <iswuzeping@163.com>
2021-12-11 13:59:41 +0800

    1211

---
+++ fronted/productMain.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+	app := iris.New()
+
+	app.HandleDir("/public", "./web/public")
+	app.HandleDir("/html", "./web/htmlProductShow")
+
+	app.Run(iris.Addr("0.0.0.0.0:80"), iris.WithoutServerError(iris.ErrServerClosed), iris.WithOptimizations)
+}
